49f7380678072a4b1fb94e1c242c85d5dede003a
zuleny <zuleny.cr@gmail.com>
2025-02-19 21:45:14 -0400

    enable cors, disable https

--- NutritionalAdvice.WebApi/Startup.cs
+++ NutritionalAdvice.WebApi/Startup.cs
@@ -45,7 +45,14 @@ namespace NutritionalAdvice.WebApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutritionalAdvice.WebApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors( builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+
+            );
+
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
