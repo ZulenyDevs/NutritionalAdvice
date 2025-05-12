30d1eec02cad4cebc0b09192deb51745eec854d2
zuleny <zuleny.cr@gmail.com>
2025-05-11 22:10:27 -0400

    test2 dp

--- NutritionalAdvice.WebApi/Startup.cs
+++ NutritionalAdvice.WebApi/Startup.cs
@@ -44,7 +44,7 @@ namespace NutritionalAdvice.WebApi
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutritionalAdvice.WebApi v4"));
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NutritionalAdvice.WebApi v5"));
 			}
 
 			app.UseCors(builder => builder
