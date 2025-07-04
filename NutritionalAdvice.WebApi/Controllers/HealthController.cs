2ac25c769027168fba1ee07af058ebf6124a6dda
Zuleny <zuleny.cr@gmail.com>
2025-07-03 20:44:33 -0400

    health endpoint

---
+++ NutritionalAdvice.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionalAdvice.WebApi.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            return Ok(new { status = "ok" });
+        }
+    }
+}
